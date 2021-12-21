44072102b2f0a54fe462474176e377140b1394ef
John Maximilian <2e0byo@gmail.com>
2021-12-21 16:53:28 +0100

    start server

---
+++ miniature_lighting_desk/server.py
@@ -0,0 +1,22 @@
+import json
+
+from . import async_hal as hal
+
+
+class Server:
+    """Server controlling a miniature lighting controller."""
+
+    def __init__(self, channels: int = 8):
+        self.controller = hal.Controller()
+        self.channels = [hal.Channel(self.controller, i) for i in range(channels)]
+        self.vals = []
+        self.sync()
+
+    def set(self, channel: int, val: int):
+        self.channels[channel].set_brightness(val)
+
+    def get(self, channel: int):
+        return self.vals[channel]
+
+    def sync(self):
+        self.vals = [channel.get_brightness() for channel in self.channels]
