39b68b0e72cb0635892c7709d2264ec4eb3bbe0a
John Maximilian <2e0byo@gmail.com>
2021-12-21 23:20:04 +0100

    rename fns

--- tests/test_server.py
+++ tests/test_server.py
@@ -22,21 +22,21 @@ def Server():
 
 
 def test_init(Server):
-    assert [Server.get(i) for i in range(8)] == [0] * 8
+    assert [Server.get_brightness(i) for i in range(8)] == [0] * 8
 
 
 def test_set(Server):
     for i in range(8):
-        assert Server.get(i) != 199
-        Server.set(i, 199)
-        assert Server.get(i) == 199
+        assert Server.get_brightness(i) != 199
+        Server.set_brightness(i, 199)
+        assert Server.get_brightness(i) == 199
 
 
 def test_jsonrpc(Server):
     s = jsonrpclib.Server(f"http://localhost:{Server.port}")
     s.sync()
-    assert s.get(0) == 0
-    s.set(1, 20)
-    assert s.get(1) == 20
+    assert s.get_brightness(0) == 0
+    s.set_brightness(1, 20)
+    assert s.get_brightness(1) == 20
     with pytest.raises(jsonrpclib.jsonrpc.ProtocolError):
         s.nosuchmethod(22)
