0633e8e0bc0c9835c1400be6b9458f88eda9edcb
John Maximilian <2e0byo@gmail.com>
2021-12-21 23:19:40 +0100

    caching = much faster

--- miniature_lighting_desk/test_server.py
+++ miniature_lighting_desk/test_server.py
@@ -7,7 +7,7 @@ from queue import Empty
 
 from .server import Server
 
-COLORS = ("red", "orange", "yellow", "green", "blue", "indigo", "violet", "brown")
+COLORS = ("red", "orange", "yellow", "green", "blue", "indigo", "black", "gold")
 
 
 class MockChannel:
@@ -35,33 +35,39 @@ class MockGuiServer(Server):
         self.root = root
         channels = kwargs.get("channels", 8)
         canvas = Canvas(root, width=channels * 50 + 2, height=62)
-        self.images = [None] * channels
+        self.images = [
+            [self.create_image(brightness, fill) for brightness in range(256)]
+            for fill in COLORS[:channels]
+        ]
+        self._current_images = [None] * channels
         canvas.pack()
         self.canvas = canvas
         for i in range(channels):
             self.set_brightness(i, 100)
 
     def set_brightness(self, channel: int, val: int):
+        val = max(0, min(val, 255))
         super().set_brightness(channel, val)
         self.queue.put((channel, val))
 
     def _set_brightness(self, channel: int, val: int):
-        self.create_rectangle(
+        self.set_image_brightness(
             1 + 50 * channel, 1, (1 + 50 * channel) + 40, 60, val, channel
         )
 
-    def create_rectangle(self, x1, y1, x2, y2, brightness, channel):
-        fill = COLORS[channel]
-        brightness = max(1, brightness)
-        self._logger.debug(f"Creating rectangle of brightness {brightness}")
+    def set_image_brightness(self, x1, y1, x2, y2, brightness, channel):
+        self._logger.debug(f"Setting channel {channel} to brightness {brightness}")
+        self._current_images[channel] = ImageTk.PhotoImage(
+            self.images[channel][brightness]
+        )
+        self.canvas.create_image(
+            x1, y1, image=self._current_images[channel], anchor="nw"
+        )
+        self.canvas.create_rectangle(x1, y1, x2, y2)
+
+    def create_image(self, brightness, fill):
         fill = self.root.winfo_rgb(fill) + (brightness,)
-        image = Image.new("RGBA", (x2 - x1, y2 - y1), fill)
-        first = not self.images[channel]
-        first = True
-        self.images[channel] = ImageTk.PhotoImage(image)
-        if first:
-            self.canvas.create_image(x1, y1, image=self.images[channel], anchor="nw")
-            self.canvas.create_rectangle(x1, y1, x2, y2)
+        return Image.new("RGBA", (40, 60), fill)
 
     def get_changes(self):
         try:
@@ -69,7 +75,7 @@ class MockGuiServer(Server):
         except Empty:
             pass
 
-        self.root.after(10, self.get_changes)
+        self.root.after(1, self.get_changes)
 
     def __enter__(self):
         super().__enter__()
