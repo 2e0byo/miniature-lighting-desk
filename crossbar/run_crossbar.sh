bd06d13ffcdfd04ccdcf92f36e3765e9e3ef7684
John Maximilian <2e0byo@gmail.com>
2021-12-26 22:47:41 +0100

    avoid overwriting PATH

--- crossbar/run_crossbar.sh
+++ crossbar/run_crossbar.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 PORT=3227
-PATH="/root/miniature-lighting-desk/crossbar"
-cd $PATH
+DIRPATH="/root/miniature-lighting-desk/crossbar"
+cd $DIRPATH
 echo "Starting crossbar"
 
 docker run -v  $PWD:/node  -v /etc/letsencrypt:/etc/letsencrypt -u 0 --rm --name=crossbar -it -p $PORT:$PORT --env-file .env crossbario/crossbar
