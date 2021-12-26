80c1f1e9bee2afa8f5b4fbf1947e166aa658ed77
John Maximilian <2e0byo@gmail.com>
2021-12-26 22:46:04 +0100

    other hack

--- crossbar/run_crossbar.sh
+++ crossbar/run_crossbar.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 PORT=3227
+PATH="/root/miniature-lighting-desk/crossbar"
+cd $PATH
 echo "Starting crossbar"
 
 docker run -v  $PWD:/node  -v /etc/letsencrypt:/etc/letsencrypt -u 0 --rm --name=crossbar -it -p $PORT:$PORT --env-file .env crossbario/crossbar
