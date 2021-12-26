a60b208c2e206ae3ff2d585b0c1028c1ea6048d8
John Maximilian <2e0byo@gmail.com>
2021-12-26 19:29:40 +0100

    use .env file

--- crossbar/run_crossbar.sh
+++ crossbar/run_crossbar.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
-
 PORT=3227
-TICKET=$(cat .crossbar-ticket)
-
 echo "Starting crossbar"
 
-CROSSBAR_TICKET=TICKET docker run -v  $PWD:/node  -v /etc/letsencrypt:/etc/letsencrypt -u 0 --rm --name=crossbar -it -p $PORT:$PORT crossbario/crossbar
+docker run -v  $PWD:/node  -v /etc/letsencrypt:/etc/letsencrypt -u 0 --rm --name=crossbar -it -p $PORT:$PORT --env-file .env crossbario/crossbar
