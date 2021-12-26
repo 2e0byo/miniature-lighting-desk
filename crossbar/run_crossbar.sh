bf0d4b858661d8f1ac5a130c52ee0380e463e0d8
John Maximilian <2e0byo@gmail.com>
2021-12-26 22:49:04 +0100

    non interactive

--- crossbar/run_crossbar.sh
+++ crossbar/run_crossbar.sh
@@ -4,4 +4,4 @@ DIRPATH="/root/miniature-lighting-desk/crossbar"
 cd $DIRPATH
 echo "Starting crossbar"
 
-docker run -v  $PWD:/node  -v /etc/letsencrypt:/etc/letsencrypt -u 0 --rm --name=crossbar -it -p $PORT:$PORT --env-file .env crossbario/crossbar
+docker run -v  $PWD:/node  -v /etc/letsencrypt:/etc/letsencrypt -u 0 --rm --name=crossbar -p $PORT:$PORT --env-file .env crossbario/crossbar
